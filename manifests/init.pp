bf47a897a81b2b8c805caabf9d04f57690cc0432
Andrew Beresford <beezly@beezly.org.uk>
2014-07-15 13:46:09 +0100

    Update to 1.5.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 # Examples
 #
 # include controlplane
-class controlplane ($version = '1.5.1') {
+class controlplane ($version = '1.5.2') {
   package {'ControlPlane':
     source   => "https://dl.dropboxusercontent.com/u/12850/ControlPlane/ControlPlane-${version}.dmg",
     provider => 'appdmg',
--- spec/classes/controlplane_spec.rb
+++ spec/classes/controlplane_spec.rb
@@ -4,7 +4,7 @@ describe 'controlplane' do
   it do
     should contain_class('controlplane')
     should contain_package('ControlPlane').with({
-      :source   => "https://dl.dropboxusercontent.com/u/12850/ControlPlane/ControlPlane-1.5.1.dmg",
+      :source   => "https://dl.dropboxusercontent.com/u/12850/ControlPlane/ControlPlane-1.5.2.dmg",
       :provider => "appdmg",
     })
   end
