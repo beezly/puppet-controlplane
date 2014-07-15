ada99b2c631853e8046b6e7efec225418b34c379
Andrew Beresford <beezly@beezly.org.uk>
2014-07-15 14:01:11 +0100

    Parameterise test version

--- spec/classes/controlplane_spec.rb
+++ spec/classes/controlplane_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe 'controlplane' do
+  version = '1.5.2'
+
   it do
     should contain_class('controlplane')
-    should contain_package('ControlPlane').with({
-      :source   => "https://dl.dropboxusercontent.com/u/12850/ControlPlane/ControlPlane-1.5.2.dmg",
+    should contain_package("ControlPlane-#{version}").with({
+      :source   => "https://dl.dropboxusercontent.com/u/12850/ControlPlane/ControlPlane-#{version}.dmg",
       :provider => "appdmg",
     })
   end
